3c4845ddc4021d4aa4ff0f6de970ac4c8191f2f1
fuglede <github@fuglede.dk>
2026-06-24 09:16:20 +0000

    Updated from GitHub Actions

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a4ce257c68517c1410f4b48ac9852ab5642a3f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2658914608" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4086188720" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
